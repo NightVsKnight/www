0437afe8cdab21b025c96ffcc8ff3b45ce4abfb7
Paul Peavyhouse <pv@swooby.com>
2018-07-11 15:26:13 -0700

    Adding fleet_night, fleet_knight, and tweaking media player in layout

--- public/audioPlayer.js
+++ public/audioPlayer.js
@@ -1,13 +1,15 @@
 
+audioPlayer();
+
 function audioPlayer() {
+
+  var playlist = $("#playlist li a");
 
   var currentAudioIndex = 0;
 
   var audioPlayer = $("#audioPlayer")[0];
-  var playlist = $("#playlist li a");
 
   audioPlayer.src = playlist[0];
-  audioPlayer.play();
 
   audioPlayer.addEventListener("ended", function() {
     currentAudioIndex++;
@@ -16,6 +18,17 @@ function audioPlayer() {
     audioPlayer.src = playlist[currentAudioIndex];
     audioPlayer.play();
   });
+
+  var promise = audioPlayer.play();
+  // console.log('audioPlayer promise=' + promise);
+  if (promise !== undefined) {
+    promise
+    .then(result => {
+      // console.log('audioPlayer: audio play succeeded: result=' + result);
+    })
+    .catch(error => {
+      console.log('audioPlayer: audio play failed: error=' + error);
+    });
+  }
 }
 
-audioPlayer();
